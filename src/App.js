c878400cf640464cfe397a1e96e629877cfc0457
zunaedsazzad <zunaedsazzad00@gmail.com>
2024-01-19 09:22:26 +0600

    Deploying to Vercel

--- src/App.js
+++ src/App.js
@@ -1,24 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
-
+import axios from 'axios';
+import { useEffect } from 'react';
+import { useState } from 'react';
+import './CustomCSS.css';
+import './CustomCSS2.css';
+import { FaQuoteLeft } from "react-icons/fa";
+import { FaRegCopyright } from "react-icons/fa";
 function App() {
+  const [quote, setquote]=useState('')
+  const [author, setauthor]=useState('')
+
+
+  const get_quote = ()=> {
+    let config = {
+      headers: {
+        'X-Api-Key': '335qrgzWRMREQYGrgrOXBQ==RjoOvuCki4ylwJDe',
+      }
+    }
+    axios.get('https://api.api-ninjas.com/v1/quotes?category=success', config).then(result=> {
+      console.log(result);
+      setquote(result.data[0].quote)
+      setauthor(result.data[0].author)
+      
+    })
+  }
+useEffect(() => {
+  get_quote()
+}, [])
+
+   
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className=" flex bg-gray-800 items-center justify-center h-screen">
+      <div>
+        <div className=" max-w-[900px] custom-font text-slate-200 text-3xl font-inter ">{quote}</div>
+        <div className="flex mt-5 justify-end text-gray-300">
+          <div className=" justify-end"><span className="text-[17px] ">said by </span>
+          <span className="custom-font2 text-lg italic">{`  ${author}`}</span></div>
+        </div>
+        
+          <div className="flex justify-center mt-10">
+          <button onClick={get_quote} className="btn-quote rounded-lg py-3 px-4 text-lg font-semibold text-yellow-200 hover:bg-green-700 hover:shadow-md border-2 flex gap-1"><FaQuoteLeft />
+            Get Another Quote
+          </button>
+
+          </div>
+          <div className="flex justify-center mt-20 items-center">
+        <FaRegCopyright size={13}/> <span className=" text-[14px]">Zunaed Sazzad</span>
+      </div>
+      </div>
+
     </div>
+
   );
 }
 
